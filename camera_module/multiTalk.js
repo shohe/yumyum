7a9a6fb234f35c350877a256b0fb59b2255c1761
Yuta Suzuki <yuta.suzuki.428.vsw@gmail.com>
2015-12-03 15:59:03 +0900

    古いjsファイルだったので更新

--- camera_module/multiTalk.js
+++ camera_module/multiTalk.js
@@ -9,7 +9,7 @@ navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
 var peer;
 
 //Callオブジェクト
-var call;
+var callObject;
 
 //MultiPartyオブジェクト
 var multiparty = null;
@@ -17,15 +17,17 @@ var multiparty = null;
 //MediaオブジェクトのURL
 var mediaObjectURL;
 
+//通話要求に対する応答
+var callPermit = false;
+
 /**
  * 通話のためのピアを設定するメソッド
  * @param id
  */
-function setUpPeer(id) {
+function setUpPeer() {
 	// Peerオブジェクトに任意のIDを振る
-	//var name = document.getElementById("phone").value;
-	var call_id = 1;
-	peer = new Peer(call_id, {
+	var name = document.getElementById("peerId").value;
+	peer = new Peer(name, {
 		key : '77196e40-7803-4e99-ac69-e0b133e711e9',
 		debug : 3
 	});
@@ -37,17 +39,26 @@ function setUpPeer(id) {
 
 	//通話要求を受信したときに呼ばれる
 	peer.on('call', function(call) {
-		var returnAnswer = confirm(call.peer + "さんからかかってきた。");
+		//var returnAnswer = confirm(call.peer + "さんからかかってきた。");
+		//var width = (screen.width - 250) / 2;
+		var width = screen.width - 285;
+		var height = (screen.height - 100) / 5.4;
+		//console.log("width : " + width);
+		//console.log("height : " + height);
+		window.open('callReceive.php?from=' + call.peer,
+					'電話が来ました。',
+					'width=250,height=100' + ',left=' + width + ',top=' + height);
 
-		//OKボタンを押して通話する場合、応答にメディアストリームをセットする
-		if (returnAnswer) {
-			call.answer(window.localStream);
-			answerCall(call);
-
-		//キャンセルして出ない場合は、強制的に通話終了
-		} else {
-			call.close();
-		}
+		callObject = call;
+//		//OKボタンを押して通話する場合、応答にメディアストリームをセットする
+//		if (callPermit) {
+//			call.answer(window.localStream);
+//			answerCall(call);
+//
+//		//キャンセルして出ない場合は、強制的に通話終了
+//		} else {
+//			call.close();
+//		}
 	});
 
 	//エラー発生時に呼ばれる
@@ -68,6 +79,23 @@ function setUpPeer(id) {
 }
 
 /**
+ * 別ウィンドウでの通話要求に対する応答を受け取るメソッド
+ */
+function callPermitCheck() {
+	console.log("callPermit:" + callPermit);
+
+	//応答が「出る」だった場合
+	if (callPermit) {
+		callObject.answer(window.localStream);
+		answerCall(callObject);
+
+	//応答が「出ない」だった場合
+	} else {
+		callObject.close();
+	}
+}
+
+/**
  * 通話要求に対する応答メソッド
  * @param call
  */
@@ -77,8 +105,10 @@ function answerCall(call) {
 		window.existingCall.close();
 	}
 
+	callObject = call;
+
 	//応答があった場合呼び出される
-	call.on('stream', function(stream) {
+	callObject.on('stream', function(stream) {
 		//メディアストリームが提供された場合（通話に出た）
 		if (stream != null) {
 			//相手の画面を表示する
@@ -88,7 +118,7 @@ function answerCall(call) {
 
 	//相手のメディアストリームがアクティブか
 	//通話を要求した方が、相手の応答を確認するための処理
-	if (call.open) {
+	if (callObject.open) {
 		setUpAndMultiCall();
 	}
 }
@@ -132,12 +162,21 @@ function multiCallProcess() {
  * 応答用メソッドを呼び出す
  */
 function makeCall(callTo) {
-	call = peer.call(callTo, window.localStream);
+	callObject = peer.call(callTo, window.localStream);
 	console.log(callTo + "さんにかけます。");
 
 	if (multiparty != null) {
 		multiCallProcess();
 	} else {
-		answerCall(call);
+		answerCall(callObject);
 	}
+}
+
+/**
+ * 通話を終了するメソッド<br>
+ * 接続を終了し、前画面に戻る
+ */
+function endCall() {
+	multiparty.close();
+	location.href = "index.php";
 }
