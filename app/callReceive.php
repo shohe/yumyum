987fdb4c42e72e3c1e6cd4b24555c693c19dfcfb
Yuta Suzuki <yuta.suzuki.428.vsw@gmail.com>
2015-12-20 15:08:51 +0900

    CSSの調整、callReceive.php変更（おそらく途中）

--- app/callReceive.php
+++ app/callReceive.php
@@ -9,23 +9,49 @@
 <head>
 	<meta charset="UTF-8">
 	<title>電話が来ました</title>
+	<link rel="stylesheet" href="./css/style.css">
 	<script type="text/javascript" src="../lib/peer/multiTalk.js" charset="UTF-8"></script>
+	<script src="../lib/tuio/jquery-1.7.2.js"></script>
+    <script src="../lib/tuio/jquery.easing.1.3.js"></script>
+    <script src="../lib/tuio/lodash.js"></script>
+    <script src="../lib/tuio/socket.io.js"></script>
+    <script src="../lib/tuio/Tuio.min.js"></script>
 </head>
 <body>
-	<p><?php echo $callFrom->getName() ?>さんから電話が来ました。</p>
-	<input type="button" onclick="callPermit();" value="出る" id="trueButton">
-	<input type="button" onclick="callCancel();" value="出ない" id="cancelButton">
+	<p id="callFromTitle"><?php echo $callFrom->getName() ?>さんから電話が来ました。</p>
+	<div id="permitButtonDiv">
+		<input type="button" value="出る" id="trueButton" class="tuio-tapEvent">
+		<input type="button" value="出ない" id="cancelButton" class="tuio-tapEvent">
+	</div>
 <script type="text/javascript">
-	function callPermit() {
-		opener.callPermit = true;
+SCREEN_W = $(document).width();
+SCREEN_H = $(document).height();
+
+$(function() {
+	var trueButton = $('#trueButton');
+    var cancelButton = $('#cancelButton');
+
+	trueButton.on('tapDown', function(e, x, y) {
 		opener.callPermitCheck('<?php echo $callFromId ?>');
 		window.close();
-	}
-	function callCancel() {
-		opener.callPermit = false;
-		opener.callPermitCheck(null);
-		window.close();
-	}
+    });
+
+    cancelButton.on('tapDown', function(e, x, y) {
+    	opener.callPermitCheck(null);
+    	window.close();
+    });
+});
+
+// 	function callPermit() {
+// 		//opener.callPermit = true;
+//		opener.callPermitCheck('<?php //echo $callFromId ?>');
+// 		window.close();
+// 	}
+// 	function callCancel() {
+// 		//opener.callPermit = false;
+// 		opener.callPermitCheck(null);
+// 		window.close();
+// 	}
 </script>
 <?php $yumDB->close() ?>
 </body>
